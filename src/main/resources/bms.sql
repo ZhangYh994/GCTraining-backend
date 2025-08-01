ad018be4ce6bf0db290488ef660bc8215045bae0
CedricZhang <cedric.zhang@developertools.com>
2025-08-01 11:47:01 +0800

    backend

--- src/main/resources/bms.sql
+++ src/main/resources/bms.sql
@@ -19,10 +19,6 @@
 -- Table structure for table `user`
 --
 
-CREATE DATABASE IF NOT EXISTS bms CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-USE bms;
-
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
